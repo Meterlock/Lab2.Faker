2eab4e4cf2e9937afd1cc2babe32cc7cfecd20ef
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-04 22:43:18 +0300

    Project created

---
+++ Faker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App is running!");
+            Console.ReadKey();
+        }
+    }
+}
