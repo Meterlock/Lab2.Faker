e672b9cf04c8aa9cd5cedc85eafd7ebcd7005d0e
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-04 23:24:50 +0300

    Interface for plugins added, Int generator implemented

---
+++ Plugins/IntGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+
+namespace Plugins
+{
+    class IntGenerator : IValueGenerator
+    {
+        public object GenerateValue()
+        {
+            return new Random().Next();
+        }
+    }
+}
