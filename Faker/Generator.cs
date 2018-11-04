9563558bd8cb1d3f9081aa23d39de6e80df6beb4
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-04 23:04:30 +0300

    Structure of Generator determined

---
+++ Faker/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class Generator
+    {
+        private Dictionary<Type, Func<object>> generatorsDict;
+
+        private void DictionaryFilling() { }
+
+        public object GenerateValue(Type type)
+        {
+            object result = null;
+            return result;
+        }
+    }
+}
