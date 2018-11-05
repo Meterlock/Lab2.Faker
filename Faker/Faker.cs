aa0e60d015a5facbcd6e60a313245bd4878c53df
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-05 16:26:55 +0300

    Structure of Faker determined

---
+++ Faker/Faker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class Faker
+    {
+        private ConstructorInfo FindBaseConstructor(Type type)
+        {
+            return null;
+        }
+
+        private ConstructorInfo FindMaxParamConstructor(Type type)
+        {
+            return null;
+        }
+
+        public object CreateByFields(Type type)
+        {
+            return null;
+        }
+
+        public object CreateByConstructor(Type type)
+        {
+            return null;
+        }
+
+        public T Create<T>()
+        {
+            Type type = typeof(T);
+            object result = null;
+            return (T)result;
+        }
+    }
+}
