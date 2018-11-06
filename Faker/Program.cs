924482f8ae5b08af2b166f1654442f9cc6b33207
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-11-06 16:18:49 +0300

    TestClass class added

--- Faker/Program.cs
+++ Faker/Program.cs
@@ -11,6 +11,12 @@ namespace Faker
         static void Main(string[] args)
         {
             Console.WriteLine("App is running!");
+
+            var test = new Generator();
+            //TestClass1 test1 = Faker.Create<TestClass1>();
+            //Console.WriteLine(test1.val1);
+            //Console.WriteLine(test1.val2);
+
             Console.ReadKey();
         }
     }
---
+++ Faker/TestClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    class TestClass1
+    {
+        public TestClass1() { }
+
+        public int val1;
+        public int val2 { get; set; }
+    }
+}
